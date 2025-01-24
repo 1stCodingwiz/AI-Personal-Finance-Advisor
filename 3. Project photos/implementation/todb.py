01d5045715c804ce44586f6f37c65ce3152bea37
Popoi <ipapaimandal@gmail.com>
2025-01-24 18:55:20 +0530

    full reports

---
+++ 3. Project photos/implementation/todb.py
@@ -0,0 +1,8 @@
+
+
+import pandas as pd
+import sqlite3 as sq
+tkr = pd.read_excel('C:\\Users\\ABC\\Desktop\\Yahoo Ticker Symbols.xlsx')
+db = sq.connect('stocks.db')
+tkr.to_sql("tkrinfo",db)
+db.close()
